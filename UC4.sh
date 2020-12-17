64c618a5634bb1ef36607ca4a91650df25306520
himanshunete <himanshuneteh@gmail.com>
2020-12-17 13:38:28 +0530

    Reach till Player gets a number 100

--- UC4.sh
+++ UC4.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash/ -x
 
 Noplay=0
@@ -5,38 +6,42 @@ Ladder=1
 Snake=2
 sum=0
 
-Win()
+Options()
 {
-   for ((i=0;i<=1000;i++))
+   diceNumber=$((RANDOM%6+1))
+   option=$((RANDOM%3))
+   if [ $option -eq $Noplay ]
+   then
+      echo NoPlay
+      echo $diceNumber
+   elif [ $option -eq $Ladder ]
+   then
+      sum=$(($sum+$diceNumber))
+      echo Player moves front to position: $sum
+   elif [ $option -eq $Snake ]
+   then
+      sum=$(($sum-$diceNumber))
+      echo Player moves back to position: $sum
+   fi
+}
+
+Win()
+{    
+   for ((i=0;i<=100;i++))
    do
-      diceNumber=$((RANDOM%6+1))
-      option=$((RANDOM%3))
-      if [ $option -eq $Noplay ]
+      Options
+      if [ $sum -eq 100 ]
       then
-         echo NoPlay
-         echo $diceNumber
-      elif [ $option -eq $Ladder ]
+         echo Player wins
+         exit
+      fi
+      if [ $sum -le 0 ]
       then
-         sum=$(($diceNumber+$sum))
-         echo Player moves front to position: $sum
-         if [ $sum -eq 100 ]
-         then
-            echo Player wins
-            exit
-         fi 
-      elif [ $option -eq $Snake ]
-      then
-         sum=$(($diceNumber-$sum))
-         if [ $sum -lt 0 ]
-         then
-            sum=0
-            echo Game restarts
-         fi
-         echo Player moves back to position: $sum
+         sum=0
+         echo Game restarts
       fi
    done
 }
 Win
-
 
 
