a18753a33a8724963a01d43b9bde5fcb8e3cd009
himanshunete <himanshuneteh@gmail.com>
2020-12-17 10:29:16 +0530

    Single Player @ position-0

--- UC1.sh
+++ UC1.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash/ -x
 
 declare -A Position 
@@ -6,17 +7,4 @@ do
    Position[$i]=0
 done
 
-echo ${Position[0]}
-
-for ((j=0;j<=50;j++))
-do
-   SinglePlayer=$((RANDOM%99+1))
-   if [ ${Position[0]} -ne 0 ]
-   then
-      Dice=$((RANDOM%6+1))            
-      ((Position[ $SinglePlayer]++))
-      Dice=$((RANDOM%6+1))      
-      echo $Dice  
-   fi
-done
-
+echo Single Players position is ${Position[0]}
