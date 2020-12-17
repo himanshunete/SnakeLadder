20a4007be397b2d4e9dda8c2ea0dfa945bf5acce
himanshunete <himanshuneteh@gmail.com>
2020-12-17 12:27:10 +0530

    Player has options NoPlay, Ladder n Snake

--- UC3.sh
+++ UC3.sh
@@ -12,15 +12,6 @@ Playerposition()
 }
 
 Playerposition
-numberOfPlayer=1
-
-diceNumber()
-{
-   diceNumber=$((RANDOM%6+1))
-   echo Player gets a dice number: $diceNumber
-}
-diceNumber
-
 
 noPlay=0
 ladder=1
