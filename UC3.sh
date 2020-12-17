215a600287ed8165321ba45ffd1d0ea29270e00b
himanshunete <himanshuneteh@gmail.com>
2020-12-17 11:53:56 +0530

    Player has options NoPlay, Ladder n Snake

---
+++ UC3.sh
@@ -0,0 +1,58 @@
+
+#!/bin/bash/ -x
+declare -A position 
+
+Playerposition()
+{
+   for((i=0;i<=100;i++))
+   do
+   position[$i]=0
+   done
+   echo Single Players position is ${position[0]}
+}
+
+Playerposition
+numberOfPlayer=1
+
+diceNumber()
+{
+   diceNumber=$((RANDOM%6+1))
+   echo Player gets a dice number: $diceNumber
+}
+diceNumber
+
+
+noPlay=0
+ladder=1
+snake=2
+sum=0
+Options()
+{  
+   for ((i=0;i<=10;i++))
+   do
+      diceNumber=$((RANDOM%6+1))
+      option=$((RANDOM%3))
+      if [ $option -eq 0 ]
+      then
+         echo NoPlay
+         echo $diceNumber
+      elif [ $option -eq 1 ]
+      then
+         diceNumber=$(($diceNumber+$sum))  
+         echo Player moves front to position: $diceNumber   
+      elif [ $option -eq 2 ]
+      then 
+         diceNumber=$(($diceNumber-$sum))
+         echo Player moves back to position: $diceNumber
+      fi
+   done 
+}
+Options
+
+
+
+
+
+
+
+
